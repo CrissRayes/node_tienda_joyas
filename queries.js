57aec974c62cf113e8edacecece11aec1858003c
Cristian Ramírez <cristian.ramirez.cl@gmail.com>
2023-06-08 22:33:53 -0400

    define pagina inicial en 1

--- queries.js
+++ queries.js
@@ -2,9 +2,9 @@ const pool = require('./db');
 const format = require('pg-format')
 
 const getJoyas = async (queryStrings) => {
-  const { limits = 3, page = 0, order_by = "precio_ASC" } = queryStrings
+  const { limits = 3, page = 1, order_by = "precio_ASC" } = queryStrings
   const [campo, direccion] = order_by.split('_')
-  const offset = page * limits
+  const offset = (page - 1) * limits
   const sqlQuery = format(`
     SELECT * FROM inventario
     ORDER BY %s %s
@@ -16,6 +16,11 @@ const getJoyas = async (queryStrings) => {
   return joyas
 }
 
+const getJoyasFiltradas = async (queryStrings) => {
+  //
+}
+
 module.exports = {
-  getJoyas
+  getJoyas,
+  getJoyasFiltradas
 }
